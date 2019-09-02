f850e2e925042582f5a6443ce0268dec05227bc5
jwkim <phoongdeng@gmail.com>
2019-09-02 15:30:57 +0900

    Django에서 axios 호출로 데이터 삭제하기

--- VueDjTodo/api/views.py
+++ VueDjTodo/api/views.py
@@ -1,5 +1,7 @@
 from django.http import JsonResponse
-from django.views.generic import ListView
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView, DeleteView
+from django.views.decorators.csrf import csrf_exempt
 
 from todo.models import Todo
 
@@ -10,3 +12,15 @@ class ApiTodoLV(ListView):
     def render_to_response(self, context, **response_kwargs):
         todoList = list(context['object_list'].values())
         return JsonResponse(data=todoList, safe=False)
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class ApiTodoDelV(DeleteView):
+    model = Todo
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.delete()
+        return JsonResponse(data={}, status=204)
+
+
